1cfa616da53aa6b94868f3c72136442577b43893
Aldomspow <55561033+Aldomspow@users.noreply.github.com>
2020-08-21 11:13:05 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-var debugThrowError = true; #default false
+var debugThrowError = false; #default false
 if (debugThrowError) {
 	// throwing an error at any startup script will cause Travis to fail
 	throw new Error();
